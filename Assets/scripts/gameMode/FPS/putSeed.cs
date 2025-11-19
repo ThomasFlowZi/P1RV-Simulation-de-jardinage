236578773eaf8feea35cca4bf6d65e45a9dc84e2
unknown <thomas.guyard34@gmail.com>
2025-11-19 15:02:55 +0100

    plantjaaj

--- Assets/scripts/gameMode/FPS/putSeed.cs
+++ Assets/scripts/gameMode/FPS/putSeed.cs
@@ -9,24 +9,12 @@ public class PutSeed : MonoBehaviour
     {
 
         if (possedeGraine)
-        
-    }
-
-
-    void Update()
-    {
-        Transform camera = Camera.main.transform;
-        RaycastHit hit;
-        
-        Ray ray = new Ray(camera.position, camera.forward);
-        if (Physics.Raycast(ray, out hit, rayDistance)  )
 
         {
+
             Instantiate(seedPrefab, gameObject.transform.position, gameObject.transform.rotation);
 
         }
-
-
+        
     }
-
 }  
